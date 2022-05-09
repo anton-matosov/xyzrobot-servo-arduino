269fcff46b6537111f6f0742f4901888a07db6fa
Anton Matosov <anton.matosov@gmail.com>
2022-05-08 19:06:52 -0700

    implement working timeout read

--- purecpp/TcpSerial.cpp
+++ purecpp/TcpSerial.cpp
@@ -71,19 +71,77 @@ uint8_t TcpSerial::peek()
     return lastRead_;
 }
 
+// template <typename SyncReadStream, typename MutableBufferSequence>
+// void readWithTimeout(SyncReadStream& s, const MutableBufferSequence& buffers, const boost::asio::deadline_timer::duration_type& expiry_time)
+// {
+//     boost::optional<boost::system::error_code> timer_result;
+//     boost::asio::deadline_timer timer(s.get_io_service());
+//     timer.expires_from_now(expiry_time);
+//     timer.async_wait([&timer_result] (const boost::system::error_code& error) { timer_result.reset(error); });
+
+//     boost::optional<boost::system::error_code> read_result;
+//     boost::asio::async_read(s, buffers, [&read_result] (const boost::system::error_code& error, size_t) { read_result.reset(error); });
+
+//     s.get_io_service().reset();
+//     while (s.get_io_service().run_one())
+//     { 
+//         if (read_result)
+//             timer.cancel();
+//         else if (timer_result)
+//             s.cancel();
+//     }
+
+//     if (*read_result)
+//         throw boost::system::system_error(*read_result);
+// }
+
+template <typename MutableBufferSequence>
+size_t read_with_timeout(boost::asio::io_service &ioService, tcp::socket &sock,
+                       const MutableBufferSequence &buffers) {
+  std::optional<boost::system::error_code> timer_result;
+  boost::asio::deadline_timer timer(ioService);
+  timer.expires_from_now(boost::posix_time::milliseconds(300));
+  timer.async_wait([&timer_result](const auto& ec) { timer_result = ec; });
+
+  std::optional<boost::system::error_code> read_result;
+  std::optional<std::size_t> bytesTransferred;
+  async_read(sock, buffers,
+             [&read_result, &bytesTransferred](const auto& ec, std::size_t bt) {
+               read_result = ec;
+               bytesTransferred = bt;
+             });
+
+  ioService.reset();
+  while (ioService.run_one()) {
+    if (read_result)
+      timer.cancel();
+    else if (timer_result)
+      sock.cancel();
+  }
+
+  if (read_result && *read_result)
+  {
+    std::cerr << read_result->message() << "\n";
+    return 0;
+    // throw boost::system::system_error(*read_result);
+  }
+
+  return *bytesTransferred;
+}
+
 uint8_t TcpSerial::read()
 {
     // if (!available()) {
     //     return kNoData;
     // }
     everRead_ = true;
-    boost::asio::read(socket_, boost::asio::buffer(&lastRead_, 1));
+    // read_with_timeout(ioService_, socket_, boost::asio::buffer(&lastRead_, 1));
     return lastRead_;
 }
 
 size_t TcpSerial::readBytes(uint8_t *buffer, size_t size)
 {
     everRead_ = true;
-    return boost::asio::read(socket_, boost::asio::buffer(buffer, size));
+    return read_with_timeout(ioService_, socket_, boost::asio::buffer(buffer, size));
 }
 
